29adfc581a946cebfbf67e5fe8592711e8a711aa
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-01-31 10:03:11 +0300

    Added trips.js

---
+++ src/trips.js
@@ -0,0 +1,51 @@
+const trips = [
+  {
+    id: 1,
+    name: "Mountains To Fjords",
+    image:
+      "https://wildlandtrekking.com/content/uploads/2020/03/Mountains-to-Fjords-Hut-to-Hut-image3-1.jpg",
+    city: "Oslo, Norway",
+    difficulty: "hard",
+    length: 59,
+    rating: 3.5,
+  },
+  {
+    id: 2,
+    name: "Iceland Winter Wonders",
+    image:
+      "https://wildlandtrekking.com/content/uploads/2020/03/Iceland-Winter-Wonders-Tour-image6-2.jpg",
+    city: "Reykjavik, Iceland",
+    difficulty: "easy",
+    length: 26,
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    name: "Mule-Assisted Classic",
+    image:
+      "https://wildlandtrekking.com/content/uploads/2020/03/magcc-slides8.jpg",
+    city: "Arizona, United States of America",
+    difficulty: "medium",
+    length: 27,
+    rating: 5,
+  },
+  {
+    id: 4,
+    name: "Tour Du Mont Blanc",
+    image:
+      "https://wildlandtrekking.com/content/uploads/2020/03/Tour-du-Mont-Blanc-image4-1.jpg",
+    city: " Geneva, Switzerland",
+    difficulty: "hard",
+    length: 82,
+    rating: 5,
+  },
+  {
+    id: 5,
+    name: "Vietnam Sapa Trek",
+    image: "https://wildlandtrekking.com/content/uploads/2020/03/image7-5.jpg",
+    city: "Hanoi, Vietnam",
+    difficulty: "easy",
+    length: 56,
+    rating: 4,
+  },
+];
